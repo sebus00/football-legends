dbbdbe5285516bba001b2a4b096085ade2bf2411
Sebastian Umiński <uminski93@gmail.com>
2020-04-26 18:01:34 +0200

    Setup project

---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MainTemplate = () => <div></div>;
+
+export default MainTemplate;
---
+++ src/views/Root.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MainTemplate from 'templates/MainTemplate';
+
+const Root = () => <MainTemplate></MainTemplate>;
+
+export default Root;
